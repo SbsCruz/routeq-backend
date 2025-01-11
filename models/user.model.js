cfe517e37d26b5e63320cf2acdffcda299d0aaca
Sebasss <sebas.cruz750@gmail.com>
2025-01-11 11:24:46 -0500

    fix: deleted pickupDate in subscription model

--- models/user.model.js
+++ models/user.model.js
@@ -7,7 +7,6 @@ const userSchema = new mongoose.Schema({
     idNumber: { type: String, required: true },
     phone: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    // TODO: userName
     password: { type: String, required: true },
 });
 
