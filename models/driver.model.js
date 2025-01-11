f4a4b0d58c577b905007c2720677efde1287b7d8
Sebasss <sebas.cruz750@gmail.com>
2025-01-11 13:20:07 -0500

    fix: deleted password field in driver and user

--- models/driver.model.js
+++ models/driver.model.js
@@ -7,7 +7,6 @@ const driverSchema = new mongoose.Schema({
     idNumber: { type: String, required: true },
     phone: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
     carId: { type: mongoose.Schema.Types.ObjectId, ref: 'Car' },
     travelNumber: {type: Number}
 });
--- models/user.model.js
+++ models/user.model.js
@@ -7,7 +7,6 @@ const userSchema = new mongoose.Schema({
     idNumber: { type: String, required: true },
     phone: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
 });
 
 module.exports = mongoose.model('User', userSchema);
