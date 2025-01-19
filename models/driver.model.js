95ecc46e77bf0d0c5ed119514097f1fd1a5fadcf
Sebasss <sebas.cruz750@gmail.com>
2025-01-19 12:32:26 -0500

    add: firebaseId into driver and user model

--- models/driver.model.js
+++ models/driver.model.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const driverSchema = new mongoose.Schema({
+    firebaseId: {tpe: String, unique: true},
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     birthDate: { type: Date, required: true },
