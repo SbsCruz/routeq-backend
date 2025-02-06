b07e97a089256207114ebefe3647158278d2916b
Sebasss <sebas.cruz750@gmail.com>
2025-02-06 17:09:11 -0500

    add: available seats to route model

--- models/route.model.js
+++ models/route.model.js
@@ -3,14 +3,15 @@ const mongoose = require('mongoose');
 const routeSchema = new mongoose.Schema({
     departureTime: { type: String, required: true },
     driverId: { type: mongoose.Schema.Types.ObjectId, ref: 'Driver' },
-    routeState: {type: Boolean, required: true},
-    startLong: {type: Number, required: true},
-    finalLong: {type: Number, required: true},
-    startLat: {type: Number, required: true},
-    finalLat: {type: Number, required: true},
-    routeName: {type: String, required: true},
-    startPoint: {type: String, required: true},
-    finalPoint: {type: String, required: true}    
+    routeState: { type: Boolean, required: true },
+    startLong: { type: Number, required: true },
+    finalLong: { type: Number, required: true },
+    startLat: { type: Number, required: true },
+    finalLat: { type: Number, required: true },
+    routeName: { type: String, required: true },
+    startPoint: { type: String, required: true },
+    finalPoint: { type: String, required: true },
+    availableSeats: { type: Number, required: true }
 });
 
 module.exports = mongoose.model('Route', routeSchema);
