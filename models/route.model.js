ca49222a016f8597cb5a78a959bc83171f6ee163
Sebasss <sebas.cruz750@gmail.com>
2025-02-05 23:14:21 -0500

    add: routeName and Points to route model

--- models/route.model.js
+++ models/route.model.js
@@ -8,6 +8,9 @@ const routeSchema = new mongoose.Schema({
     finalLong: {type: Number, required: true},
     startLat: {type: Number, required: true},
     finalLat: {type: Number, required: true},
+    routeName: {type: String, required: true},
+    startPoint: {type: String, required: true},
+    finalPoint: {type: String, required: true}    
 });
 
 module.exports = mongoose.model('Route', routeSchema);
