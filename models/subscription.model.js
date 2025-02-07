fdc80ae9adcf52ea357a136919263c39e877a28c
Sebasss <sebas.cruz750@gmail.com>
2025-02-07 08:33:55 -0500

    feat: routeId not a foreign key

--- models/subscription.model.js
+++ models/subscription.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const subscriptionSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    routeId: { type: mongoose.Schema.Types.ObjectId, ref: 'Route', required: true },
+    routeId: { type: String, required: true },
     subscriptionDate: { type: Date, default: Date.now },
     pickUpLat: {type: Number, required: true},
     pickUpLong: {type: Number, required: true},
